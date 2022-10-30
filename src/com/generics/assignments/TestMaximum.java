d267c1aec064dfee8181d42551edb140dc97f98e
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-30 20:14:53 +0530

    Added : Use Case 1 Program - Find out maximum from given three integers.

--- src/com/generics/assignments/TestMaximum.java
+++ src/com/generics/assignments/TestMaximum.java
@@ -1,4 +1,8 @@
+//Use Case 1 is to give 3 integers and find out the maximum and its position in array.
+
 package com.generics.assignments;
+
+import java.util.Scanner;
 
 public class TestMaximum {
 
@@ -11,5 +15,47 @@ public class TestMaximum {
     public static void main(String[] args) {
         //Initialization of object.
         TestMaximum testMaximum = new TestMaximum();
+
+        //Define the array where size is 3(given).
+        Integer[] array = addToArray();
+        printArray(array);
+        getMaximum(array);
+    }
+
+    //Get maximum from the array and its position.
+    public static void getMaximum(Integer[] array) {
+        int max = array[0];
+        int position = 0;
+        for (int i = 0; i < array.length; i++)
+        {
+            int a = array[i].compareTo(max);
+            if(a > 0)
+            {
+                max = array[i];
+                position = i;
+            }
+        }
+        System.out.println("\nMaximum integer is : "+max);
+        System.out.println("Maximum integer position is : "+position+" in array.");
+    }
+
+    //Printing to array.
+    public static void printArray(Integer[] array) {
+        System.out.println("Array is : ");
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i]+"  ");
+        }
+    }
+
+    //Adding values to array.
+    public static Integer[] addToArray() {
+        System.out.println("Enter 3 values");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        return new Integer[]{a,b,c};
     }
 }
